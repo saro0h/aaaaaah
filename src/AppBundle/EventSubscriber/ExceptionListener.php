f508e07f06cad5acdd37ce93cae0fb948d3b5b96
Sarah Khalil <mkhalil.sarah@gmail.com>
2017-03-08 01:15:09 +0100

    --

---
+++ src/AppBundle/EventSubscriber/ExceptionListener.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace AppBundle\EventSubscriber;
+
+use AppBundle\Normalizer\NormalizerInterface;
+use JMS\Serializer\Serializer;
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpKernel\Event\GetResponseForExceptionEvent;
+use Symfony\Component\HttpKernel\KernelEvents;
+
+class ExceptionListener implements EventSubscriberInterface
+{
+    private $serializer;
+    private $normalizers;
+
+    public function __construct(Serializer $serializer)
+    {
+        $this->serializer = $serializer;
+    }
+
+    public static function getSubscribedEvents()
+    {
+        return array(
+            KernelEvents::EXCEPTION => [['processException', 255]]
+        );
+    }
+
+    public function processException(GetResponseForExceptionEvent $event)
+    {
+        foreach ($this->normalizers as $normalizer) {
+            $result = $normalizer->normalize($event->getException());
+        }
+
+        $body = $this->serializer->serialize($result['body'], 'json');
+
+        $event->setResponse(new Response($body, $result['code']));
+    }
+
+    public function addNormalizer(NormalizerInterface $normalizer)
+    {
+        $this->normalizers[] = $normalizer;
+    }
+}
